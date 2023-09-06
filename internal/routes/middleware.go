919c69f41c204c428f3bba2ff63844907d7abecd
dmawardi <dmawardi@gmail.com>
2023-09-06 11:25:28 +0800

    Fixed CORS middleware to adhere to convention

--- internal/routes/middleware.go
+++ internal/routes/middleware.go
@@ -7,14 +7,18 @@ import (
 // Middleware that adds CORS headers to every response
 func corsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Set headers to allow cross-origin requests.
+		w.Header().Set("Access-Control-Allow-Origin", "*") // Replace "*" with allowed origins.
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		// Handle preflight requests (OPTIONS method).
 		if r.Method == http.MethodOptions {
-			// For OPTIONS requests, set the CORS header to allow all origins
-			w.Header().Set("Access-Control-Allow-Origin", "*")
-			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
-			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-			w.WriteHeader(http.StatusOK)
+			w.WriteHeader(http.StatusNoContent)
 			return
 		}
+
+		// Call the next handler.
 		next.ServeHTTP(w, r)
 	})
 }
