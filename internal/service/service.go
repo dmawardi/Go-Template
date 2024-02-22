ee3a0fca62298314cf5932f88c92bbde305af43f
dmawardi <dmawardi@gmail.com>
2024-02-22 16:49:29 +0800

    Migrated main in service to service.go. Removed useless code

--- internal/service/service.go
+++ internal/service/service.go
@@ -5,16 +5,7 @@ import "github.com/dmawardi/Go-Template/internal/config"
 // Repository used by handler package
 var app *config.AppConfig
 
-// // Repository type
-// type Repository struct {
-// 	App *config.AppConfig
-// }
-
 // Create new service repository
 func BuildServiceState(a *config.AppConfig) {
 	app = a
-}
-
-type Services struct {
-	User *UserService
 }
