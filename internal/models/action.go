8ce8179f9acd5da87691b17b1b658bfaea9854e1
Dannar Mawardi <dannar@Dannars-MacBook-Pro.local>
2024-08-08 19:09:16 +0800

    Added CreateAction and UpdateAction in models and cleaned up db schema for Action

---
+++ internal/models/action.go
@@ -0,0 +1,17 @@
+package models
+
+type CreateAction struct {
+	ActionType  string `json:"action_type,omitempty" valid:"required"`
+	EntityType  string `json:"entity_type,omitempty" valid:"required"`
+	EntityID    uint   `json:"entity_id,omitempty" valid:"required"`
+	Changes     string `json:"changes,omitempty" valid:""`
+	Description string `json:"description,omitempty" valid:""`
+}
+
+type UpdateAction struct {
+	ActionType  string `json:"action_type,omitempty" valid:""`
+	EntityType  string `json:"entity_type,omitempty" valid:""`
+	EntityID    uint   `json:"entity_id,omitempty" valid:""`
+	Changes     string `json:"changes,omitempty" valid:""`
+	Description string `json:"description,omitempty" valid:""`
+}
---
+++ internal/service/core/action.go
@@ -0,0 +1,6 @@
+package coreservices
+
+// func newBasicModuleService[dbSchema, create, update any](repo corerepositories.ActionRepository) BasicModuleService[dbSchema, create, update] {
+// 	return &basicModuleService[dbSchema, create, update]{
+
+// }
