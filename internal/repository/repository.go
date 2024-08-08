2f09f3ddd337f137e11055b3b3b1cda796ab7c8d
Dannar Mawardi <dannar@Dannars-MacBook-Pro.local>
2024-08-08 20:25:22 +0800

    Added a basic module service to modules in service. Added basic generic repository

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"github.com/dmawardi/Go-Template/internal/config"
+	"github.com/dmawardi/Go-Template/internal/models"
 	corerepositories "github.com/dmawardi/Go-Template/internal/repository/core"
 )
 
@@ -11,4 +12,13 @@ func SetAppConfig(appConfig *config.AppConfig) {
 	app = appConfig
 	// Set app config in repository
 	corerepositories.SetAppConfig(app)
+}
+
+type BasicModuleRepository[dbSchema any] interface {
+	FindAll(limit int, offset int, order string, conditions []models.QueryConditionParameters) (*models.BasicPaginatedResponse[dbSchema], error)
+	FindById(int) (*dbSchema, error)
+	Create(entity *dbSchema) (*dbSchema, error)
+	Update(int, *dbSchema) (*dbSchema, error)
+	Delete(int) error
+	BulkDelete([]int) error
 }
--- internal/service/core/action.go
+++ internal/service/core/action.go
@@ -1,6 +1,2 @@
 package coreservices
 
-// func newBasicModuleService[dbSchema, create, update any](repo corerepositories.ActionRepository) BasicModuleService[dbSchema, create, update] {
-// 	return &basicModuleService[dbSchema, create, update]{
-
-// }
